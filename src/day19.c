d7b458f43ae6c2c298ff100ac6c94794b313c0c7
CapitaineToinon <capitainetoinon@proton.me>
2026-05-26 01:22:42 +0200

    Done with day 19

--- src/day19.c
+++ src/day19.c
@@ -1,4 +1,5 @@
 #include <regex.h>
+#include <stdint.h>
 #include <stdio.h>
 #include <stdlib.h>
 #include <string.h>
@@ -6,10 +7,15 @@
 #define MAX_RULES 100
 #define MAX_LINE 1000
 #define MAX_RULE_LEN 10
+#define INPUT_REG_GROUP 2
+#define RULE_REG_GROUP 3
 
 // Note, all strings in these structs
 // are not null terminalted, hence the <name>_len
 // fields.
+
+int min(int a, int b) { return a < b ? a : b; }
+int max(int a, int b) { return a > b ? a : b; }
 
 typedef struct rule {
   char *from;
@@ -19,14 +25,14 @@ typedef struct rule {
 } rule_t;
 
 typedef struct context {
-  rule_t *rules;
+  rule_t **rules;
   int rules_len;
   char *input;
   int input_len;
 } context_t;
 
 typedef struct cache {
-  int *hashes;
+  uint64_t *hashes;
   int size;
 } cache_t;
 
@@ -37,7 +43,24 @@ cache_t *create_cache() {
   return cache;
 }
 
-bool is_cached(int hash, cache_t *cache) {
+void free_cache(cache_t *cache) {
+  if (cache->hashes != NULL) {
+    free(cache->hashes);
+  }
+
+  free(cache);
+}
+
+void shuffle(rule_t **rules, int n) {
+  for (int i = 0; i < n - 1; i++) {
+    size_t j = i + rand() / (RAND_MAX / (n - i) + 1);
+    rule_t *t = rules[j];
+    rules[j] = rules[i];
+    rules[i] = t;
+  }
+}
+
+bool is_cached(uint64_t hash, cache_t *cache) {
   for (int i = 0; i < cache->size; i++) {
     if (cache->hashes[i] == hash) {
       return true;
@@ -47,26 +70,49 @@ bool is_cached(int hash, cache_t *cache) {
   return false;
 }
 
-bool add_cache(int hash, cache_t *cache) {
+bool add_cache(uint64_t hash, cache_t *cache) {
   if (is_cached(hash, cache)) {
     return false;
   }
 
-  cache->hashes = realloc(cache->hashes, sizeof(int) * (cache->size + 1));
+  cache->hashes = realloc(cache->hashes, sizeof(uint64_t) * (cache->size + 1));
   cache->hashes[cache->size] = hash;
   cache->size++;
 
   return true;
 }
 
-int djb2(char *input, int input_len) {
-  int hash = 5381;
+uint64_t djb2(char *input, int input_len) {
+  uint64_t hash = 5381;
 
   for (int i = 0; i < input_len; i++) {
     hash = ((hash << 5) + hash) + input[i];
   }
 
   return hash;
+}
+
+void invert_rules(rule_t **rules, int rules_len) {
+  for (int i = 0; i < rules_len; i++) {
+    rule_t *r = rules[i];
+
+    char *tmp = r->from;
+    r->from = r->to;
+    r->to = tmp;
+
+    int tmp_len = r->from_len;
+    r->from_len = r->to_len;
+    r->to_len = tmp_len;
+  }
+}
+
+char *regmatch_to_string(char *line, regmatch_t *m, int *output_len) {
+  int len = m->rm_eo - m->rm_so;
+  char *output = malloc(sizeof(char) * len);
+  memcpy(output, line + m->rm_so, len);
+
+  *output_len = len;
+  return output;
 }
 
 context_t *get_context() {
@@ -91,49 +137,41 @@ context_t *get_context() {
   ctx->rules_len = 0;
 
   while (fgets(line, MAX_LINE, file)) {
-    regmatch_t rules_match[3];
-    regmatch_t input_match[2];
+    regmatch_t rules_match[RULE_REG_GROUP];
+    regmatch_t input_match[INPUT_REG_GROUP];
 
-    if (regexec(&rules_reg, line, 3, rules_match, 0) == 0) {
-      int from_len = rules_match[1].rm_eo - rules_match[1].rm_so;
-      int to_len = rules_match[2].rm_eo - rules_match[2].rm_so;
-
-      ctx->rules = realloc(ctx->rules, sizeof(rule_t) * (ctx->rules_len + 1));
-      ctx->rules[ctx->rules_len].from = malloc(sizeof(char) * from_len);
-      ctx->rules[ctx->rules_len].to = malloc(sizeof(char) * to_len);
-      memcpy(ctx->rules[ctx->rules_len].from, line + rules_match[1].rm_so,
-             from_len);
-      memcpy(ctx->rules[ctx->rules_len].to, line + rules_match[2].rm_so,
-             to_len);
-      ctx->rules[ctx->rules_len].from_len = from_len;
-      ctx->rules[ctx->rules_len].to_len = to_len;
-      ctx->rules_len++;
+    if (regexec(&rules_reg, line, RULE_REG_GROUP, rules_match, 0) == 0) {
+      rule_t *rule = malloc(sizeof(rule_t));
+      rule->from = regmatch_to_string(line, &rules_match[1], &rule->from_len);
+      rule->to = regmatch_to_string(line, &rules_match[2], &rule->to_len);
+      ctx->rules = realloc(ctx->rules, sizeof(rule_t *) * (ctx->rules_len + 1));
+      ctx->rules[ctx->rules_len++] = rule;
       continue;
     }
 
-    if (regexec(&input_reg, line, 2, input_match, 0) == 0) {
-      int input_len = input_match[1].rm_eo - input_match[1].rm_so;
-      ctx->input_len = input_len;
-      ctx->input = malloc(sizeof(char) * ctx->input_len);
-      memcpy(ctx->input, line + input_match[1].rm_so, ctx->input_len);
+    if (regexec(&input_reg, line, INPUT_REG_GROUP, input_match, 0) == 0) {
+      ctx->input = regmatch_to_string(line, &input_match[1], &ctx->input_len);
       continue;
     }
   }
+
+  fclose(file);
+  regfree(&rules_reg);
+  regfree(&input_reg);
 
   return ctx;
 }
 
-void print_context(context_t *ctx) {
+void free_context(context_t *ctx) {
   for (int i = 0; i < ctx->rules_len; i++) {
-    printf("%.*s -> %.*s\n", ctx->rules[i].from_len, ctx->rules[i].from,
-           ctx->rules[i].to_len, ctx->rules[i].to);
+    free(ctx->rules[i]->from);
+    free(ctx->rules[i]->to);
+    free(ctx->rules[i]);
   }
 
-  printf("\n%.*s (%d)\n", ctx->input_len, ctx->input, ctx->input_len);
-}
-
-bool input_match(char *input, rule_t *rule) {
-  return memcmp(input, rule->from, rule->from_len) == 0;
+  free(ctx->rules);
+  free(ctx->input);
+  free(ctx);
 }
 
 char *apply_rule(char *input, int input_len, int pos, rule_t *rule,
@@ -141,100 +179,92 @@ char *apply_rule(char *input, int input_len, int pos, rule_t *rule,
   int next_len = input_len - rule->from_len + rule->to_len;
   char *next = malloc(next_len);
 
-  if (next == NULL) {
-    printf("failed to malloc\n");
-    exit(EXIT_FAILURE);
-  }
-
   memcpy(next, input, pos);
   memcpy(next + pos, rule->to, rule->to_len);
   memcpy(next + pos + rule->to_len, input + pos + rule->from_len,
-         input_len - pos - 1);
+         input_len - pos - rule->from_len);
 
   *output_len = next_len;
   return next;
 }
 
-int solve(char *input, int input_len, rule_t *rules, int rules_len) {
-  cache_t *cache = create_cache();
-
-  for (int pos = 0; pos < input_len; pos++) {
-    for (int r = 0; r < rules_len; r++) {
-      if (input_match(input + pos, &rules[r])) {
-        int next_len = 0;
-        char *next = apply_rule(input, input_len, pos, &rules[r], &next_len);
-        int hash = djb2(next, next_len);
-        (void)add_cache(hash, cache);
-      }
-    }
-  }
-
-  return cache->size;
+char *copy_string(char *s, int len) {
+  char *copy = malloc(sizeof(char) * len);
+  memcpy(copy, s, len);
+  return copy;
 }
 
-int solve_p2(char *input, int input_len, int target_hash, int target_len,
-             int steps, rule_t *rules, int rules_len, cache_t *cache) {
+int solve(context_t *ctx) {
+  cache_t *cache = create_cache();
 
-  if (input_len > target_len) {
-    return -1;
-  }
-
-  int current_hash = djb2(input, input_len);
-
-  if (current_hash == target_hash) {
-    return steps;
-  }
-
-  if (add_cache(current_hash, cache) == false) {
-    // already tries this input
-    return -1;
-  }
-
-  for (int pos = 0; pos < input_len; pos++) {
-    for (int r = 0; r < rules_len; r++) {
-      if (input_match(input + pos, &rules[r])) {
-        // printf("found match for %.*s -> %.*s as pos %d\n", rules[r].from_len,
-        //        input + pos, rules[r].to_len, rules[r].to, pos);
+  for (int i = 0; i < ctx->rules_len; i++) {
+    rule_t *r = ctx->rules[i];
+    for (int pos = 0; pos <= ctx->input_len - r->from_len; pos++) {
+      if (memcmp(ctx->input + pos, r->from, r->from_len) == 0) {
         int next_len = 0;
-        char *next = apply_rule(input, input_len, pos, &rules[r], &next_len);
-
-        // printf("next: %.*s\n", next_len, next);
-
-        int solution = solve_p2(next, next_len, target_hash, target_len,
-                                steps + 1, rules, rules_len, cache);
-
+        char *next = apply_rule(ctx->input, ctx->input_len, pos, r, &next_len);
+        int hash = djb2(next, next_len);
+        (void)add_cache(hash, cache);
         free(next);
-
-        if (solution != -1) {
-          return solution;
-        }
       }
     }
   }
 
-  return -1;
+  int result = cache->size;
+  free_cache(cache);
+  return result;
+}
+
+int funny(context_t *ctx) {
+  while (true) {
+    int cur_len = ctx->input_len;
+    char *cur = copy_string(ctx->input, ctx->input_len);
+    int steps = 0;
+    bool stuck = false;
+
+    while (!(cur_len == 1 && cur[0] == 'e')) {
+      bool found = false;
+      for (int i = 0; i < ctx->rules_len && !found; i++) {
+        rule_t *r = ctx->rules[i];
+        for (int pos = 0; pos <= cur_len - r->from_len && !found; pos++) {
+          if (memcmp(cur + pos, r->from, r->from_len) == 0) {
+            char *next = apply_rule(cur, cur_len, pos, r, &cur_len);
+            free(cur);
+            steps++;
+            cur = next;
+            found = true;
+          }
+        }
+      }
+
+      if (!found) {
+        stuck = true;
+        break;
+      }
+    }
+
+    if (!stuck) {
+      free(cur);
+      return steps;
+    }
+
+    // Try again
+    free(cur);
+    shuffle(ctx->rules, ctx->rules_len);
+  }
 }
 
 int main() {
   context_t *ctx = get_context();
-  print_context(ctx);
 
-  {
-    int part_1 = solve(ctx->input, ctx->input_len, ctx->rules, ctx->rules_len);
-    printf("%d\n", part_1);
-  }
+  int part_1 = solve(ctx);
+  printf("%d\n", part_1);
 
-  {
-    cache_t *cache = create_cache();
-    int target_hash = djb2(ctx->input, ctx->input_len);
-    char *start = "e";
-    int start_len = 1;
+  invert_rules(ctx->rules, ctx->rules_len);
+  int part_2 = funny(ctx);
+  printf("%d\n", part_2);
 
-    int part_2 = solve_p2(start, start_len, target_hash, ctx->input_len, 0,
-                          ctx->rules, ctx->rules_len, cache);
-
-    printf("%d\n", part_2);
-  }
+  free_context(ctx);
 
   return 0;
 }
